dfd67b529d2a8078be7a9265cf006ccd06090656
Dileep Venepally <dileepvenapally@gmail.com>
2026-03-13 10:33:10 +0530

    updated login page

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher(['/login(.*)']);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) await auth.protect();
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
+  }
 });
 
 export const config = {
